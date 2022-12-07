4a53a92cd8f230d50f5fe4651a0303696abe42ad
Milan NICOLAS <milan.nicolas@hotmail.fr>
2022-12-07 21:14:45 +0100

    Dependencies udpate
    Updating dependencies for security reasons

--- dist/js/background.js
+++ dist/js/background.js
@@ -11,14 +11,13 @@ chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.get(function (result) {
     console.log(result); // Print existing storage data to the service worker console
   });
+
   InitStorage();
 });
-
 function InitStorage() {
   chrome.storage.sync.get(function (result) {
     chrome.storage.sync.set({
       time: result.time === undefined ? 15 : result.time // Set default time = 15 if not set
-
     });
   });
 }
